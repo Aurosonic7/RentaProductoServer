86834d6ca14d786a3ecf2c8a102d51c7a1ebe7a5
Aurosonic7 <99984328+Aurosonic7@users.noreply.github.com>
2024-11-10 01:19:54 -0600

    Remove Dropbox token from .env.development

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from 'express';
 import * as productoController from '../controllers/producto.controller.js';
 import verifyToken from '../middlewares/auth.js';
@@ -15,18 +14,5 @@ router.route('/:id')
   .delete(verifyToken, productoController.delete_producto)
   .put(verifyToken, upload.single('imagen'), productoController.update_producto);
 
-=======
-import { Router } from "express";
-import * as productosController from '../controllers/productos.controller.js';
-import verifyToken from '../middlewares/auth.js';
-
-const router = Router();
-
-router.route('/').get(verifyToken, productosController.get_productos);
-router.route('/').post(verifyToken, productosController.create_producto);
-router.route('/:id').get(verifyToken, productosController.get_producto);
-router.route('/:id').put(verifyToken, productosController.update_producto);
-router.route('/:id').delete(verifyToken, productosController.delete_producto);
->>>>>>> refs/remotes/origin/develop
 
 export default router;
